11715fc39162fd122a0b26ba0b7e49e270e42e8d
felix <ramzes767@gmail.com>
2017-12-28 22:45:56 +0200

    наборы представлений и маршрутизаторы

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
+# -*- coding: utf-8 -*-
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 from snippets import views
 
-# API endpoints
-urlpatterns = format_suffix_patterns([
-    url(r'^$', views.api_root),
-    url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    url(r'^users/$', views.UserList.as_view(), name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail')
-])
+# Создание маршрутизатора и регистрация наших представлений с ним.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# Теперь URL-адреса API автоматически определяются маршрутизатором.
+urlpatterns = [
+    url(r'^', include(router.urls))
+]
